311b682190898a73a46730a0922afc41838cb21f
alesq <46975854+alesq@users.noreply.github.com>
2024-02-15 20:18:13 +0100

    Delete _plugins directory

--- _plugins/random_number.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  class RandomNumberTag < Liquid::Tag
-    def render(context)
-      rand(1..100) # Generate a random number between 1 and 100
-    end
-  end
-end
-
-Liquid::Template.register_tag('random_number', Jekyll::RandomNumberTag)
